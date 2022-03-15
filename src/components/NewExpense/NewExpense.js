05c6c62f3b3e930ef3fa99bad16a330addf42060
Shreyas Dixit <shreyas.dixit97@gmail.com>
2022-03-15 23:42:49 +0530

    Assignment to add Cancel and Add New Exp button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,34 @@
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css'
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+import { useState } from "react/cjs/react.development";
 
 const NewExpense = (props) => {
+  const [isVisible, setIsVisible] = useState(false);
+
   const saveNewExpenseDataHandler = (currentExpenseData) => {
     const expenseData = {
       ...currentExpenseData,
-      id: Math.random().toString()
-    }
-    props.onAddNewExpense(expenseData)
-  }
+      id: Math.random().toString(),
+    };
+    props.onAddNewExpense(expenseData);
+    setIsVisible(!isVisible)
+  };
+
+  const hideNewExpenseDataForm = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div className='new-expense'>
+    <div className="new-expense">
       {/* add an event handler to the custom component */}
-      <ExpenseForm onSaveNewExpenseData={saveNewExpenseDataHandler} />
+      {isVisible ? (
+        <ExpenseForm
+          onSaveNewExpenseData={saveNewExpenseDataHandler}
+          onCancelNewExpense={hideNewExpenseDataForm}
+        />
+      ) : (
+        <button onClick={hideNewExpenseDataForm}>Add New Expense</button>
+      )}
     </div>
   );
 };
